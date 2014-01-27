d8d0ee11d8f32de48482cc7501ccd18b63ddabec
Pablo Benitez <pablo@ebizmarts.com>
2014-01-27 12:19:31 -0200

    cartExpire and send payment failed email.

--- app/code/local/Ebizmarts/SagePaySuite/Helper/Checkout.php
+++ app/code/local/Ebizmarts/SagePaySuite/Helper/Checkout.php
@@ -8,23 +8,19 @@
  * @author      Ebizmarts Team <info@ebizmarts.com>
  */
 
-class Ebizmarts_SagePaySuite_Helper_Checkout extends Mage_Core_Helper_Abstract
-{
+class Ebizmarts_SagePaySuite_Helper_Checkout extends Mage_Core_Helper_Abstract {
 
-	public function getOnepage()
-	{
+	public function getOnepage() {
 		return Mage::getSingleton('checkout/type_onepage');
 	}
 
-	public function isMultiShippingOverview()
-	{
+	public function isMultiShippingOverview() {
 		$request = Mage::app()->getRequest();
 
 		return (bool)('/checkout/multishipping/overview/' === $request->getRequestString());
 	}
 
-    public function deleteQuote()
-    {
+    public function deleteQuote() {
         if($this->getOnepage()->getQuote()->hasItems())
         {
             try {
@@ -34,5 +30,26 @@ class Ebizmarts_SagePaySuite_Helper_Checkout extends Mage_Core_Helper_Abstract
                 Mage::logException($e);
             }
         }
+    }
+
+    public function cartExpire($quote) {
+
+        $expire = false;
+
+        if( !$quote->hasItems()
+            || $quote->getHasError()
+            || $quote->getIsMultiShipping()
+            || Mage::getSingleton('checkout/session')->getCartWasUpdated()
+            || !Mage::getSingleton('sagepaysuite/session')->getLastVendorTxCode()) {
+
+            $expire = true;
+
+        }
+
+        return $expire;
+    }
+
+    public function sendPaymentFailedEmail($quote, $message) {
+        Mage::helper('checkout')->sendPaymentFailedEmail($quote, $message);
     }
 }
