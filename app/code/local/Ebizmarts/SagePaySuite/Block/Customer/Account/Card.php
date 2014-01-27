fcf44efe568f3c2738bfcfbd0e4f0bfd19d2ade7
Pablo Benitez <pablo@ebizmarts.com>
2014-01-27 12:20:54 -0200

    load customer cards by integration

--- app/code/local/Ebizmarts/SagePaySuite/Block/Customer/Account/Card.php
+++ app/code/local/Ebizmarts/SagePaySuite/Block/Customer/Account/Card.php
@@ -6,18 +6,31 @@
  * @package    Ebizmarts_SagePaySuite
  * @author     Ebizmarts <info@ebizmarts.com>
  */
-class Ebizmarts_SagePaySuite_Block_Customer_Account_Card extends Mage_Core_Block_Template
-{
+class Ebizmarts_SagePaySuite_Block_Customer_Account_Card extends Mage_Core_Block_Template {
+
 	protected $_cards = null;
 
-	public function getCustomerCards()
-	{
+	public function getCustomerCards() {
         if (is_null($this->_cards)) {
 
-            $this->_cards = $this->helper('sagepaysuite/token')->loadCustomerCards();
+        	$config = (string) Mage::getStoreConfig('payment/sagepaysuite/token_integration', Mage::app()->getStore()->getId());
+
+        	$method = null;
+
+        	if($config == 'server') {
+        		$method = 'sagepayserver';
+        	}
+        	else {
+        		if($config == 'direct') {
+					$method = 'sagepaydirectpro';
+        		}
+        	}
+
+            $this->_cards = $this->helper('sagepaysuite/token')->loadCustomerCards($method);
 
         }
 
         return $this->_cards;
 	}
+
 }
