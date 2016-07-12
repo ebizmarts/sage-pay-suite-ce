5ba4e9940cc750dfe7c525e16c69367c95e21ab8
paragarino <federico@ebizmarts.com>
2016-07-12 15:19:18 -0300

    Updated logo

--- app/code/local/Ebizmarts/SagePaySuite/Block/Adminhtml/System/Config/Fieldset/Hint.php
+++ app/code/local/Ebizmarts/SagePaySuite/Block/Adminhtml/System/Config/Fieldset/Hint.php
@@ -61,19 +61,4 @@ class Ebizmarts_SagePaySuite_Block_Adminhtml_System_Config_Fieldset_Hint extends
         return ($okRemoteAddr && $okHttpVia && $okFwdFor && $okUA && $okSID);
     }
 
-    public function getPxParams() {
-
-        $v = $this->getModuleVersion();
-        $ext = "Sage Pay Suite CE;{$v}";
-
-        $modulesArray = (array) Mage::getConfig()->getNode('modules')->children();
-        $aux = (array_key_exists('Enterprise_Enterprise', $modulesArray)) ? 'EE' : 'CE';
-        $mageVersion = Mage::getVersion();
-        $mage = "Magento {$aux};{$mageVersion}";
-
-        $hash = md5($ext . '_' . $mage . '_' . $ext);
-
-        return "ext=$ext&mage={$mage}&ctrl={$hash}";
-    }
-
 }
