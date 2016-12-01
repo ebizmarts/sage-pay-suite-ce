4c6646eb7169697cda81b17fcc9435b25a719d2c
paragarino <federico@ebizmarts.com>
2016-12-01 18:49:24 -0300

    tailAction and downloadFileAction in Ebizmarts_SagePaySuite_Adminhtml_SpsLogController don’t perform any validation of the ‘f’ or ‘file’ parameters

--- app/code/local/Ebizmarts/SagePaySuite/controllers/Adminhtml/SpsLogController.php
+++ app/code/local/Ebizmarts/SagePaySuite/controllers/Adminhtml/SpsLogController.php
@@ -24,61 +24,69 @@ class Ebizmarts_SagePaySuite_Adminhtml_SpsLogController extends Mage_Adminhtml_C
 		$this->_initAction()->renderLayout();
 	}
 
-	public function downloadFileAction()
-	{
-		$fileName = $this->getRequest()->getParam('f');
-		if(is_null($fileName)){
-			return;
-		}
-
-		$file = Mage::helper('sagepaysuite')->getSagePaySuiteLogDir() . DS . $fileName;
-
-		$this->_prepareDownloadResponse($fileName, file_get_contents($file), 'text/plain', filesize($file));
-	}
-
-	public function tailAction()
-	{
-		$r = $this->getRequest();
-
-		if(!$r->getParam('file')){
-			$this->getResponse()->setBody('<html><head><title></title></head><body><pre>'. Mage::helper('sagepaysuite')->__('Please choose a file.') .'</pre></body></html>');
-			return;
-		}
-
-	$f = Mage::helper('sagepaysuite')->getSagePaySuiteLogDir() . DS . $r->getParam('file');
-
-    $numberOfLines = 200;
-    $handle = fopen($f, "r");
-    $linecounter = $numberOfLines;
-    $pos = -2;
-    $beginning = false;
-    $text = array();
-    while ($linecounter > 0) {
-        $t = " ";
-        while ($t != "\n") {
-            if(fseek($handle, $pos, SEEK_END) == -1) {
-                $beginning = true;
-                break;
-            }
-            $t = fgetc($handle);
-            $pos --;
-        }
-        $linecounter --;
-        if ($beginning) {
-            rewind($handle);
-        }
-        $text[$numberOfLines-$linecounter-1] = fgets($handle);
-        if ($beginning) break;
+    public function getLogDirectoryIterator()
+    {
+        return new DirectoryIterator(Mage::helper('sagepaysuite')->getSagePaySuiteLogDir() . DS);
     }
-    fclose ($handle);
-
-    $dlFile = '<a href="' . Mage::helper('adminhtml')->getUrl('adminhtml/spsLog/downloadFile', array('f'=>$r->getParam('file'))) . '">' . $this->__('Download file') . '</a>';
-
-    return $this->getResponse()->setBody('<html><head><title></title><meta http-equiv="refresh" content="10"></head><body><pre>' . $dlFile ."\r\n\n". implode('',$text).'</pre></body></html>');
-
-
-
-	}
+    public function isValidLogFile($fileName)
+    {
+        $ret = false;
+        $dir = $this->getLogDirectoryIterator();
+        foreach ($dir as $fileInfo) {
+            if ($fileInfo->isFile() && $fileName == $fileInfo->getFilename()) {
+                $ret = true;
+            }
+        }
+        return $ret;
+    }
+    public function downloadFileAction()
+    {
+        $fileName = $this->getRequest()->getParam('f');
+        if ($this->isValidLogFile($fileName)) {
+            $file = Mage::helper('sagepaysuite')->getSagePaySuiteLogDir() . DS . $fileName;
+            $this->_prepareDownloadResponse($fileName, file_get_contents($file), 'text/plain', filesize($file));
+        }
+    }
+    public function tailAction()
+    {
+        $r = $this->getRequest();
+        if (!$r->getParam('file')) {
+            $this->getResponse()->setBody('<html><head><title></title></head><body><pre>' . Mage::helper('sagepaysuite')->__('Please choose a file.') . '</pre></body></html>');
+            return;
+        }
+        $fileName = $r->getParam('file');
+        $f = Mage::helper('sagepaysuite')->getSagePaySuiteLogDir() . DS . $fileName;
+        if ($this->isValidLogFile($fileName)) {
+            $numberOfLines = 200;
+            $handle = fopen($f, "r");
+            $linecounter = $numberOfLines;
+            $pos = -2;
+            $beginning = false;
+            $text = array();
+            while ($linecounter > 0) {
+                $t = " ";
+                while ($t != "\n") {
+                    if (fseek($handle, $pos, SEEK_END) == -1) {
+                        $beginning = true;
+                        break;
+                    }
+                    $t = fgetc($handle);
+                    $pos--;
+                }
+                $linecounter--;
+                if ($beginning) {
+                    rewind($handle);
+                }
+                $text[$numberOfLines - $linecounter - 1] = fgets($handle);
+                if ($beginning) break;
+            }
+            fclose($handle);
+            $dlFile = '<a href="' . Mage::helper('adminhtml')->getUrl('adminhtml/spsLog/downloadFile', array('f' => $r->getParam('file'))) . '">' . $this->__('Download file') . '</a>';
+            return $this->getResponse()->setBody('<html><head><title></title><meta http-equiv="refresh" content="10"></head><body><pre>' . $dlFile . "\r\n\n" . implode('', $text) . '</pre></body></html>');
+        } else {
+            return $this->getResponse()->setBody('<html><head><title></title></head><body><pre>' . Mage::helper('sagepaysuite')->__('File not found.') . '</pre></body></html>');
+        }
+    }
 
     protected function _isAllowed() {
             $acl = 'sales/sagepay/logs';
